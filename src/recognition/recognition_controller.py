8270c24f6a87af3e2e34bcf1a8703c1d0164dcc0
Maxson Almeida <maxsonferovante@gmail.com>
2024-09-17 23:30:34 -0300

    Adicionar endpoint para upload de arquivos de reconhecimento com validação

--- src/recognition/recognition_controller.py
+++ src/recognition/recognition_controller.py
@@ -1,8 +1,9 @@
 from nest.core import Controller, Get, Post
-
+from fastapi import UploadFile
 from .recognition_service import RecognitionService
 from .recognition_model import Recognition
-
+from .recognition_validation import validate_file_type, validate_file_size
+from .recognition_exceptions import InternalServerError
 
 @Controller("recognition")
 class RecognitionController:
@@ -14,7 +15,17 @@ class RecognitionController:
     async def get_recognition(self):
         return await self.recognition_service.get_recognition()
 
-    @Post("/")
-    async def add_recognition(self, recognition: Recognition):
-        return await self.recognition_service.add_recognition(recognition)
- 
+    @Post("/upload")
+    async def add_recognition(self, file: UploadFile):        
+        validate_file_size(file)
+        validate_file_type(file)
+            
+        file_name = file.filename        
+        recognition = Recognition(file_name=file_name)        
+        recognition_id = await self.recognition_service.add_recognition(recognition)
+        return {
+                "recognition_id": recognition_id,
+                "file": file.filename,
+                "size": len(file.file.read()),
+                "extension": file.filename.split(".")[-1]
+            }
