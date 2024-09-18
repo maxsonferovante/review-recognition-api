4acc31b10475e112281c18b36c4faf37dc7947f0
Maxson Almeida <maxsonferovante@gmail.com>
2024-09-17 23:30:43 -0300

    Adicionar campos 'created_at' e 'id' à entidade de reconhecimento e normalizar o nome do arquivo

--- src/recognition/recognition_model.py
+++ src/recognition/recognition_model.py
@@ -1,6 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
+import uuid
+import unicodedata 
 
+
+def genarete_id():
+    return str(uuid.uuid4())
 
 class Recognition(BaseModel):
-    name: str
+    created_at: datetime = Field(init=False, default_factory=datetime.now)
+    file_name: str
+    id: str = Field(init=False, default_factory=genarete_id)    
+    
+    
+    def __post_init__(self):
+        self.__normalize_file_name()
+        
+    def __normalize_file_name(self):
+        self.file_name.lower().replace(" ", "_")
+        self.file_name = unicodedata.normalize('NFKD', self.file_name).encode('ASCII', 'ignore').decode('utf-8')
+        
+        
+        
+    
 
