4fb956cd4b187e034385c20fbe8656dfe27656f5
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-02 01:55:39 -0300

    Refatorar middleware de validação de reconhecimento para usar cabeçalhos de requisição e simplificar validações de tipo e tamanho de arquivo

--- src/middlewares/recognition_validation_middleware.py
+++ src/middlewares/recognition_validation_middleware.py
@@ -12,31 +12,33 @@ MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB
 class RecognitionValidationMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI):
         super().__init__(app)
-        # lista de validações
-        self.validations = [validate_file_type, validate_file_size]
         
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
         if request.url.path == "/recognition/upload" and request.method == "POST":
-            file = await request.form()
-            for validation in self.validations:
-                error = validation(file["file"])
+            errors = []            
+            
+            errors.append(validate_file_size(file_content=request.headers.get("content-length")))
+            
+            # errors.append(validate_file_type(content_type=request.headers.get("content-type")))
+            
+            for error in errors:
                 if error is not None:
                     return JSONResponse(
                         status_code=error.status_code,
                         content=error.detail
                     )
-            # request.form = file
-        # 
+            
+            
+            # file.file.seek(0) 
+            pass
         return response
 
-def validate_file_type(file: UploadFile):
-    if file.content_type not in ALLOWED_EXTENSIONS:
-        return FileTypeNotAllowed()
-    return None
-def validate_file_size(file: UploadFile):
-    file_size = len(file.file.read())
-    file.file.seek(0)  # Volta para o início do arquivo após leitura
-    if file_size > MAX_FILE_SIZE:        
+def validate_file_type(content_type: str = None):
+    if content_type not in ALLOWED_EXTENSIONS:
+        raise FileTypeNotAllowed()
+    
+def validate_file_size(file_content = None):
+    if int(file_content) > MAX_FILE_SIZE:        
         return FileSizeExceeded()
     return None
