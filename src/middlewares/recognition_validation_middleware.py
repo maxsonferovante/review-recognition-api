d9c7da63e5e5b4f4efc7ba84bc4daeb9288192e7
Maxson Almeida <maxsonferovante@gmail.com>
2024-10-01 21:08:48 -0300

    Adicionar validações de tipo e tamanho de arquivo no middleware de reconhecimento

--- src/middlewares/recognition_validation_middleware.py
+++ src/middlewares/recognition_validation_middleware.py
@@ -1,4 +1,5 @@
-from fastapi import UploadFile
+from fastapi import UploadFile, Request, FastAPI
+from fastapi.responses import StreamingResponse
 from src.modules.recognition.recognition_exceptions import FileTypeNotAllowed, FileSizeExceeded
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse  # Importar JSONResponse
@@ -9,28 +10,24 @@ ALLOWED_EXTENSIONS = {'application/pdf'}
 MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB
 
 class RecognitionValidationMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app):
+    def __init__(self, app: FastAPI):
         super().__init__(app)
         # lista de validações
         self.validations = [validate_file_type, validate_file_size]
         
-    async def dispatch(self, request, call_next):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
         if request.url.path == "/recognition/upload" and request.method == "POST":
             file = await request.form()
             for validation in self.validations:
                 error = validation(file["file"])
-                if error:
-                    """ 
-                    error é uma instância de FileTypeNotAllowed ou FileSizeExceeded,
-                    mas recebo o erro:    await response(scope, wrapped_receive, send)
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-TypeError: 'FileSizeExceeded' object is not callable
-                    """
+                if error is not None:
                     return JSONResponse(
                         status_code=error.status_code,
                         content=error.detail
                     )
-        response = await call_next(request)
+            # request.form = file
+        # 
         return response
 
 def validate_file_type(file: UploadFile):
